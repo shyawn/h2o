879cf3ac4b8eb8a68972cd54a41b971eaa47abf9
mattChrisP <matthewchristopherpohadi@gmail.com>
2023-03-20 21:26:51 +0800

    small adjustment

--- testing.py
+++ testing.py
@@ -1,6 +1,4 @@
-import subprocess 
-subprocess.call(["g++", "testcode.c"]) 
+import subprocess
 subprocess.run(["gcc", "-o", "testing", "--coverage", "testing.c"])
-# subprocess.run(["./testing"])
+subprocess.run(["./testing"])
 subprocess.run(["gcov", "testing.c", "-m"])
-subprocess.run(["cat", "testing.c.gcov"])
