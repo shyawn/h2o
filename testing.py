9f36f4760e4757b931feed2d35d645c2860ad5a6
ShawnCsj <csjshawn@gmail.com>
2023-03-21 10:01:34 +0800

    edit testing.py

--- testing.py
+++ testing.py
@@ -2,3 +2,4 @@ import subprocess
 subprocess.run(["gcc", "-o", "testing", "--coverage", "testing.c"])
 subprocess.run(["./testing"])
 subprocess.run(["gcov", "testing.c", "-m"])
+# subprocess.run(["cat", "testing.c.gcov"])
